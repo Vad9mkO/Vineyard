d17ba7b1fa8d88619702ea355b043376b2f1419c
Vad9mkO <vadimsuslov@rambler.ru>
2018-05-20 22:22:35 +0300

    .

--- src/main/java/com/vineyard/courseproject/configuration/LocalRedisConfiguration.java
+++ src/main/java/com/vineyard/courseproject/configuration/LocalRedisConfiguration.java
@@ -28,39 +28,44 @@ public class LocalRedisConfiguration {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
 
 //
-//        redisStandaloneConfiguration.setHostName("redis-14262.c17.us-east-1-4.ec2.cloud.redislabs.com");//localhost
-//                redisStandaloneConfiguration.setPort(14262);//6379
-//                redisStandaloneConfiguration.setDatabase(0);
-//                redisStandaloneConfiguration.setPassword(RedisPassword.of("QKZshWLJIRsdekGePibLtLWfjWy5Ueet"));
-        try {
-            //REDISCLOUD_URL
-            //REDIS_URL - heroku redis
+        redisStandaloneConfiguration.setHostName("ec2-34-235-90-46.compute-1.amazonaws.com");//localhost
+        redisStandaloneConfiguration.setPort(18529);//6379
+//        redisStandaloneConfiguration.setDatabase(0);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of("p792925e8eb45eabde52403ae0402534671d5550cdb93ee204dbd59e879143c0b"));
 
-            Optional<String> redisUrl = Optional.ofNullable(System.getenv("REDIS_URL"));
+        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+        jedisClientConfiguration.connectTimeout(Duration.ofSeconds(60));// 60s connection timeout
 
-            if(redisUrl.isPresent()) {
-                URI redisUri = new URI(redisUrl.get());
-//                mysql://be875fd7939285:1624c5ca@us-cdbr-iron-east-04.cleardb.net/heroku_b714bc5a91769ff?reconnect=true
-//                redis://rediscloud:QKZshWLJIRsdekGePibLtLWfjWy5Ueet@redis-14262.c17.us-east-1-4.ec2.cloud.redislabs.com:14262
-
-                redisStandaloneConfiguration.setHostName(redisUri.getHost());//localhost
-                redisStandaloneConfiguration.setPort(redisUri.getPort());//6379
-                redisStandaloneConfiguration.setDatabase(0);
-                redisStandaloneConfiguration.setPassword(RedisPassword.of(redisUri.getUserInfo().split(":", 2)[1]));
-            } else {
-                redisStandaloneConfiguration.setHostName("localhost");//localhost
-                redisStandaloneConfiguration.setPort(6379);//6379
-            }
-
-
-//            JedisPool pool = new JedisPool()
-
-//            redisStandaloneConfiguration.setDatabase(0);
-
-            JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
-            jedisClientConfiguration.connectTimeout(Duration.ofSeconds(60));// 60s connection timeout
-
-            return jedisConnectionFactory;
+        return jedisConnectionFactory;
+//        try {
+//            //REDISCLOUD_URL
+//            //REDIS_URL - heroku redis
+//
+//            Optional<String> redisUrl = Optional.ofNullable(System.getenv("REDIS_URL"));
+//
+//            if(redisUrl.isPresent()) {
+//                URI redisUri = new URI(redisUrl.get());
+////                mysql://be875fd7939285:1624c5ca@us-cdbr-iron-east-04.cleardb.net/heroku_b714bc5a91769ff?reconnect=true
+////                redis://rediscloud:QKZshWLJIRsdekGePibLtLWfjWy5Ueet@redis-14262.c17.us-east-1-4.ec2.cloud.redislabs.com:14262
+//
+////                redisStandaloneConfiguration.setHostName(redisUri.getHost());//localhost
+////                redisStandaloneConfiguration.setPort(redisUri.getPort());//6379
+////                redisStandaloneConfiguration.setDatabase(0);
+////                redisStandaloneConfiguration.setPassword(RedisPassword.of(redisUri.getUserInfo().split(":", 2)[1]));
+//            } else {
+//                redisStandaloneConfiguration.setHostName("localhost");//localhost
+//                redisStandaloneConfiguration.setPort(6379);//6379
+//            }
+//
+//
+////            JedisPool pool = new JedisPool()
+//
+////            redisStandaloneConfiguration.setDatabase(0);
+//
+//            JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+//            jedisClientConfiguration.connectTimeout(Duration.ofSeconds(60));// 60s connection timeout
+//
+//            return jedisConnectionFactory;
 //
 //
 // SPRING BOOT 1.5.13
@@ -77,9 +82,9 @@ public class LocalRedisConfiguration {
 //            /** SPRING BOOT 2.0.2*/
 //
 //             RedisStandaloneConfiguratu
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return null;
-        }
+//        } catch (URISyntaxException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
     }
 }
